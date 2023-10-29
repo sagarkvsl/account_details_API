625f17a739e8f8a6477d93dfac0c9857ea19d4fb
CodeSandbox <support@codesandbox.io>
2023-10-29 19:23:59 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import requests
 
 app = Flask(__name__)
 
+# Replace 'YOUR_API_KEY' with your actual API key
+API_KEY = 'YOUR_API_KEY'
+API_URL = 'https://api.brevo.com/v3/account'
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        api_key = request.form.get('api_key')
+        if api_key:
+            # Use the provided API key
+            headers = {'api-key': api_key}
+        else:
+            # Use the default API key
+            headers = {'api-key': API_KEY}
+        
+        response = requests.get(API_URL, headers=headers)
 
+        if response.status_code == 200:
+            data = response.json()
+            return render_template('response.html', data=data)
+        else:
+            return f"Error: {response.status_code}"
+    
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
